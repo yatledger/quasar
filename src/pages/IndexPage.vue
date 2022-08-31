ac1512b28d84bb9f7d9b21a3649fc8379883b7e2
YZoZ <mail@yzoz.com>
2022-09-01 02:55:58 +0300

    password

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -36,27 +36,32 @@ const pinDecrypt = async (p, m) => {
   return r
 }
 
-const pin = ref('')
+const pwd = ref('')
+const pwdRef = ref(null)
 const pk = ref('')
+const isPwd = ref(true)
 
 const mn = generateMnemonic(russian).normalize('NFKD')
 const seed = pbkdf2(sha512, mn, '', { c: 2048, dkLen: 32 })
 const k = sign.keyPair.fromSeed(seed)
 pk.value = b58.binary_to_base58(k.publicKey)
 
-const enter = async () => {
-  const enc = await pinEncrypt(pin.value, mn)
+const next = async () => {
+  pwdRef.value.validate()
+  if (pwdRef.value.hasError) {
+    return
+  }
+  const enc = await pinEncrypt(pwd.value, mn)
   console.log(b58.binary_to_base58(enc))
-  const dec = await pinDecrypt(pin.value, enc)
+  const dec = await pinDecrypt(pwd.value, enc)
   const decoder = new TextDecoder()
   const encoder = new TextEncoder()
   console.log(decoder.decode(dec))
 
-  const nonce = new Uint8Array(24)
-  const k = pbkdf2(sha512, pin.value, '', { c: 2048, dkLen: 32 })
-  const sec = secretbox(encoder.encode(mn), nonce, k)
+  const k = pbkdf2(sha512, pwd.value, '', { c: 2048, dkLen: 32 })
+  const sec = secretbox(encoder.encode(mn), new Uint8Array(24), k)
   console.log(b58.binary_to_base58(sec))
-  const unsec = secretbox.open(sec, nonce, k)
+  const unsec = secretbox.open(sec, new Uint8Array(24), k)
   console.log(decoder.decode(unsec))
 }
 
@@ -69,8 +74,31 @@ const enter = async () => {
       <p class="text-h4">{{ mn }}</p>
       <p class="text-left text-body1">А для взаимодействия со мной и остальными участниками у тебя уже есть свой публичный адрес, который вывелся напрямую из цифровой подписи:</p>
       <p class="text-subtitle1">{{ pk }}</p>
-      <q-input v-model="pin" filled counter maxlength="4" placeholder="Четыре цифры" hint="Чтобы защитить подпись" mask="####" label="PIN-код" />
-      <q-btn color="primary" size="xl" @click="enter" label="Войти" />
+      <p class="text-left text-body1">Осталось только ввести и запомнить надёжный</p>
+      <q-input
+        ref="pwdRef"
+        v-model="pwd"
+        filled
+        counter
+        :type="isPwd ? 'password' : 'text'"
+        placeholder="Цифры, буквы, знаки"
+        hint="Чтобы защитить подпись"
+        label="Пароль"
+        lazy-rules
+        :rules="[
+          val => !!val || 'Лучше введите пароль!',
+          val => val.length > 6 || 'Лучше ведите больше 6 символов',
+        ]"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+      <q-btn color="primary" size="xl" @click="next" label="Продолжить" />
     </div>
   </q-page>
 </template>
