13a79fe68697eda861000ef53777e9329a6878fe
YZoZ <mail@yzoz.com>
2023-01-29 15:51:11 +0300

    template

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,45 @@ useClient({
 <template>
   <router-view />
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+
+h2 {
+  font-family: 'Roboto Slab', serif;
+  font-size: 20px;
+  font-weight: normal;
+  margin-bottom: 1vh
+}
+
+p {
+  font-family: 'Roboto Slab', serif;
+  font-size: 15px;
+  font-weight: normal;
+  margin-bottom: 0.5vh;
+  line-height: 20px;
+}
+
+a {
+  color: #ac5de7;
+  text-decoration: underline;
+}
+
+p,
+li {
+  margin-bottom: 10px;
+}
+
+ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%
+}
+
+.raw_txt {
+  white-space: pre-line;
+  word-wrap: break-word;
+}
+</style>
