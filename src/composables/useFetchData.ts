0f6a1410f83b757d432ab364e7fef52894479807
Antonbinom <protest_the_hero@mail.ru>
2024-02-09 15:39:54 +0530

    fix: useFtechData

--- src/composables/useFetchData.ts
+++ src/composables/useFetchData.ts
@@ -1,117 +1,132 @@
-import { useQuery } from '@vue/apollo-composable'
-import gql from 'graphql-tag'
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
 
 const useFetchData = () => {
-
   const getAllUsers = () => {
     const getDataQuery = gql`
-    query getUsers {
-      allUsers {
-        id
-        name
-        image
-        balance
-        description
-        contacts
+      query getUsers {
+        allUsers {
+          id
+          name
+          image
+          balance
+          description
+          contacts
+        }
       }
-    }
-  `
-    return useQuery(getDataQuery)
-  }
+    `;
+    return useQuery(getDataQuery);
+  };
 
   const getUserById = (id: number) => {
     const getDataQuery = gql`
-    query getUserById ($id: ID!) {
-      user (id: $id) {
-        id
-        name
-        image
-        balance
-        description
+      query getUserById($id: ID!) {
+        user(id: $id) {
+          id
+          name
+          image
+          balance
+          description
+          contacts
+        }
       }
-    }
-  `
-    return useQuery(getDataQuery, { id })
-  }
+    `;
+    return useQuery(getDataQuery, { id });
+  };
 
   const getAllTransactions = (userId: number) => {
     const getDataQuery = gql`
-    query getTransactions($userId: ID!) {
-      allTransactions(userId: $userId) {
-        id
-        amount
-        debit
-        message
-        sender {
+      query getTransactions($userId: ID!) {
+        allTransactions(userId: $userId) {
           id
-          name
-        }
-        recipient {
-          id
-          name
+          amount
+          debit
+          message
+          sender {
+            id
+            name
+          }
+          recipient {
+            id
+            name
+          }
+          created_at
         }
       }
-    }
-  `
-    return useQuery(getDataQuery, { userId })
-  }
+    `;
+    return useQuery(getDataQuery, { userId });
+  };
 
   const getTransactionById = (id: number) => {
     const getDataQuery = gql`
-    query getTransactionById ($id: ID!) {
-      transaction (id: $id) {
-        id
-        amount
-        debit
-        message
-        sender
-        recipient
+      query getTransactionById($id: ID!) {
+        transaction(id: $id) {
+          id
+          amount
+          debit
+          message
+          sender {
+            id
+            name
+          }
+          recipient {
+            id
+            name
+          }
+          created_at
+        }
       }
-    }
-  `
-    return useQuery(getDataQuery, { id })
-  }
+    `;
+    return useQuery(getDataQuery, { id });
+  };
 
   const getUserTransactions = (userId: number) => {
     const getDataQuery = gql`
-    query getUserTransactions ($userId: ID!){
-      allUserTransactions (userId: $userId) {
-        id
-        amount
-        debit
-        message
-        sender {
+      query getUserTransactions($userId: ID!) {
+        allUserTransactions(userId: $userId) {
           id
-          name
-        }
-        recipient {
-          id
-          name
+          amount
+          debit
+          message
+          sender {
+            id
+            name
+          }
+          recipient {
+            id
+            name
+          }
+          created_at
         }
       }
-    }
-    `
-    return useQuery(getDataQuery, { userId })
-  }
+    `;
+    return useQuery(getDataQuery, { userId });
+  };
 
   const allUserContacts = (userId: number) => {
     const getDataQuery = gql`
-    query getUserContacts ($userId: ID!){
-      allUserContacts (userId: $userId) {
-        id
-        name
-        image
-        balance
-        description
+      query getUserContacts($userId: ID!) {
+        allUserContacts(userId: $userId) {
+          id
+          name
+          image
+          description
+        }
       }
-    }
-    `
-    const transactions = useQuery(getDataQuery, { userId })
+    `;
+    const transactions = useQuery(getDataQuery, { userId });
 
-    return transactions
-  }
+    return transactions;
+  };
 
-  return { getAllUsers, getUserById, getUserTransactions, getTransactionById, getAllTransactions, allUserContacts }
+  return {
+    getAllUsers,
+    getUserById,
+    getUserTransactions,
+    getTransactionById,
+    getAllTransactions,
+    allUserContacts,
+  };
 };
 
-export default useFetchData
+export default useFetchData;
