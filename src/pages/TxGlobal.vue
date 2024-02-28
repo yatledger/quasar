4a570726b95d5a46a50924b5ba7348c465ff9a6e
habdevs <habdevs@yandex.ru>
2024-02-29 00:54:39 +0500

    feat: update table layout | added from to usert

--- src/pages/TxGlobal.vue
+++ src/pages/TxGlobal.vue
@@ -14,20 +14,19 @@ const initialTransactions = [
     toUser: {
       id: 2,
       name: 'Bob',
-      avatar: 'yatb-s1.png',
+      avatar: 'icon-144x144.png',
       wallet: 'wallet_B'
     },
-    amount: 0.0025, // Биткоины, например
-    credit: 'BTC',
-    debit: 'ETH',
+    amount: 0.0025,
+    ticket: 'YAT',
     msg: 'Payment for services',
-    time: '2024-02-19T12:30:00'
+    time: '2024-02-19T12:30:19'
   },
   {
     fromUser: {
       id: 3,
       name: 'Charlie',
-      avatar: 'yatb-s1.png',
+      avatar: 'icon-144x144.png',
       wallet: 'wallet_C'
     },
     toUser: {
@@ -37,10 +36,315 @@ const initialTransactions = [
       wallet: 'wallet_D'
     },
     amount: 1.5, // Эфириумы, например
-    credit: 'ETH',
-    debit: 'BTC',
+    ticket: 'YAT',
     msg: 'Product purchase',
-    time: '2024-02-19T14:45:00'
+    time: '2024-02-19T14:45:18'
+  },
+  {
+    fromUser: {
+      id: 12,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 22,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:17'
+  },
+  {
+    fromUser: {
+      id: 32,
+      name: 'Charlie',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_C'
+    },
+    toUser: {
+      id: 42,
+      name: 'David',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_D'
+    },
+    amount: 1.5, // Эфириумы, например
+    ticket: 'YAT',
+    msg: 'Product purchase',
+    time: '2024-02-19T14:45:16'
+  },
+  {
+    fromUser: {
+      id: 13,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 23,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:15'
+  },
+  {
+    fromUser: {
+      id: 33,
+      name: 'Charlie',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_C'
+    },
+    toUser: {
+      id: 43,
+      name: 'David',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_D'
+    },
+    amount: 1.5, // Эфириумы, например
+    ticket: 'YAT',
+    msg: 'Product purchase',
+    time: '2024-02-19T14:45:14'
+  },
+  {
+    fromUser: {
+      id: 14,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 24,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:13'
+  },
+  {
+    fromUser: {
+      id: 34,
+      name: 'Charlie',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_C'
+    },
+    toUser: {
+      id: 44,
+      name: 'David',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_D'
+    },
+    amount: 1.5, // Эфириумы, например
+    ticket: 'YAT',
+    msg: 'Product purchase',
+    time: '2024-02-19T14:45:12'
+  },
+  {
+    fromUser: {
+      id: 15,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 25,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:11'
+  },
+  {
+    fromUser: {
+      id: 36,
+      name: 'Charlie',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_C'
+    },
+    toUser: {
+      id: 46,
+      name: 'David',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_D'
+    },
+    amount: 1.5, // Эфириумы, например
+    ticket: 'YAT',
+    msg: 'Product purchase',
+    time: '2024-02-19T14:45:10'
+  },
+  {
+    fromUser: {
+      id: 17,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 27,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:09'
+  },
+  {
+    fromUser: {
+      id: 38,
+      name: 'Charlie',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_C'
+    },
+    toUser: {
+      id: 48,
+      name: 'David',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_D'
+    },
+    amount: 1.5, // Эфириумы, например
+    ticket: 'YAT',
+    msg: 'Product purchase',
+    time: '2024-02-19T14:45:08'
+  },
+  {
+    fromUser: {
+      id: 171,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 271,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:07'
+  },
+  {
+    fromUser: {
+      id: 172,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 272,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:06'
+  },
+  {
+    fromUser: {
+      id: 173,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 273,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:05'
+  },
+  {
+    fromUser: {
+      id: 174,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 274,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:04'
+  },
+  {
+    fromUser: {
+      id: 175,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 275,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:03'
+  },
+  {
+    fromUser: {
+      id: 176,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 276,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:02'
+  },
+  {
+    fromUser: {
+      id: 177,
+      name: 'Alice',
+      avatar: 'yatb-s1.png',
+      wallet: 'wallet_A'
+    },
+    toUser: {
+      id: 277,
+      name: 'Bob',
+      avatar: 'icon-144x144.png',
+      wallet: 'wallet_B'
+    },
+    amount: 0.0025,
+    ticket: 'YAT',
+    msg: 'Payment for services',
+    time: '2024-02-19T12:30:01'
   }
 ]
 
@@ -48,8 +352,7 @@ const { data, error } = useQuery(gql`
 query getTx {
     tx(amount: -1000, skip: 25, limit: 15) {
       amount
-      credit
-      debit
+      ticket
       msg
       time
       fromUser {
@@ -66,6 +369,7 @@ if (error) {
 }
 
 const transactions = ref(initialTransactions)
+const getAvatarUrl = (avatar) => `src/assets/${avatar}`
 
 // Заполняем массив транзакций данными из запроса GraphQL
 if (data && data.tx) {
@@ -74,32 +378,30 @@ if (data && data.tx) {
 
 const columns = [
   {
-    name: 'avatars',
-    label: 'Avatars',
+    name: 'fromUserAvatars',
+    label: 'From',
     align: 'left',
-    field: 'avatars'
+    field: 'fromUser.avatar'
   },
   {
     name: 'amount',
     label: 'Amount',
-    align: 'left',
+    align: 'center',
     field: 'amount',
     sortable: true,
     format: (val) => Math.round(val).toString() // Преобразование в целое число
   },
   {
-    name: 'credit',
-    label: 'Credit',
+    name: 'toUserAvatars',
+    label: 'To',
     align: 'center',
-    field: 'credit',
-    sortable: true
+    field: 'toUser.avatar'
   },
   {
-    name: 'debit',
-    label: 'Debit',
-    align: 'right',
-    field: 'debit',
-    sortable: true
+    name: 'Ticket',
+    label: 'Ticket',
+    align: 'center',
+    field: 'ticket'
   },
   {
     name: 'msg',
@@ -121,17 +423,23 @@ const columns = [
 
 <template>
   <div class="q-pa-md my-padding">
-    <q-table class=" my-sticky-dynamic" style="height: calc(100vh - 100px)" flat bordered title="Транзакции"
-      :rows="transactions" :columns="columns" row-key="time">
-      <template v-slot:body-cell-avatars="props">
-        <q-avatar>
-          <img :src="'https://cdn.quasar.dev/img/avatar.png'" alt="from"
-            style="width: 20px; height: 20px; border-radius: 50%;">
-        </q-avatar>
-        <q-avatar>
-          <img :src="'/assets/' + props.row.toUser.avatar" alt="to"
-            style="width: 20px; height: 20px; border-radius: 50%;">
-        </q-avatar>
+    <q-table class=" my-sticky-dynamic" style="height: calc(100vh - 100px)" flat bordered dense dark title="Транзакции"
+      :rows="transactions" :columns="columns" row-key="time" :rows-per-page-options="[10, 15, 20, 25, 50, 0]">
+      <template v-slot:body-cell-fromUserAvatars="props">
+        <q-td :props="props">
+          <q-avatar>
+            <img v-if="props.col.field === 'fromUser.avatar'" :src="getAvatarUrl(props.row.fromUser.avatar)" alt="from"
+              style="max-width: 40px; max-height: 40px; border-radius: 50%;">
+          </q-avatar>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-toUserAvatars="props">
+        <q-td :props="props">
+          <q-avatar>
+            <img v-if="props.col.field === 'toUser.avatar'" :src="getAvatarUrl(props.row.toUser.avatar)" alt="to"
+              style="max-width: 40px; max-height: 40px; border-radius: 50%;">
+          </q-avatar>
+        </q-td>
       </template>
     </q-table>
   </div>
@@ -159,7 +467,7 @@ const columns = [
     /* height of all previous header rows */
     scroll-margin-top: 48px
 
-  .my-padding
+.my-padding
   @media (max-width: $breakpoint-sm-max)
     padding-left: 0 !important
     padding-right: 0 !important
