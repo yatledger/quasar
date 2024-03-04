1cce80c7ac6e35d06978b8e4aad9c2964e989235
MaxSkr0210 <ily0210skr@gmail.com>
2024-03-04 08:49:39 +0300

    new features

--- quasar.config.js
+++ quasar.config.js
@@ -31,7 +31,10 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
       'i18n',
-      'apollo'
+      'apollo',
+      'db',
+      'crypt',
+      'utils'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
---
+++ src/boot/crypt.js
@@ -0,0 +1,72 @@
+import { boot } from 'quasar/wrappers'
+import { sign, randomBytes } from 'tweetnacl'
+
+export default boot(({ app }) => {
+  const crypt = {
+
+    b2h (b) {
+      return Array.prototype.map.call(b, function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2)
+      }).join('')
+    },
+
+    h2b (h) {
+      if (typeof h !== 'string') throw new TypeError('expected string')
+      const b = new Uint8Array(h.length / 2)
+      let i = 0
+      while (h.length >= 2) {
+        b[i] = parseInt(h.substring(0, 2), 16)
+        h = h.substring(2, h.length)
+        i++
+      }
+      return b
+    },
+
+    u2b (s) {
+      if (typeof s !== 'string') throw new TypeError('expected string')
+      const d = unescape(encodeURIComponent(s)), b = new Uint8Array(d.length)
+      for (let i = 0; i < d.length; i++) b[i] = d.charCodeAt(i)
+      return b
+    },
+
+    b2u (b) {
+      const s = []
+      for (let i = 0; i < b.length; i++) s.push(String.fromCharCode(b[i]))
+      return decodeURIComponent(escape(s.join('')))
+    },
+
+    async genKey () {
+      return this.b2h(randomBytes(32))
+    },
+
+    async recKey (s) {
+      let k = ''
+      if (s.length > 64) {
+        k = sign.keyPair.fromSecretKey(this.h2b(s)) // legacy
+      } else {
+        k = sign.keyPair.fromSeed(this.h2b(s))
+      }
+      const sec = k.secretKey // PRIVATE KEY
+      const pub = k.publicKey // PUBLIC KEY
+      return [this.b2h(sec), this.b2h(pub)]
+    },
+
+    // mes, sec
+    sign (m, s) {
+      return this.b2h(sign(this.u2b(m), this.h2b(s)))
+    },
+
+    // mes, pub
+    unsign (m, p) {
+      const o = sign.open(this.h2b(m), this.h2b(p))
+
+      if (o) {
+        return this.b2u(o)
+      } else {
+        return false
+      }
+    }
+  }
+
+  app.config.globalProperties.$crypt = crypt
+})
