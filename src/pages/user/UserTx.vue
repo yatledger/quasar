c472595246167ff8c1bd61923b43c1f194f28601
Nadar <nadar.veselov@mail.ru>
2024-03-04 15:32:47 +0300

    Update UserTx.vue and UserTxHistory.vue add q-zar component q-table

--- src/pages/user/UserTx.vue
+++ src/pages/user/UserTx.vue
@@ -1,105 +1,138 @@
 <template>
-  <h5>Транзакции</h5>
   <section>
-    <UserHistoryTx :records="records" />
+    <UserHistoryTx :records="records" :columns="columns"/>
   </section>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
 import UserHistoryTx from '../../components/UserTxHistory.vue'
 
-export default {
-  name: 'UserTx',
-  data: () => ({
-    records: [
-      {
-        date: '21.01.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 999.01,
-        type: 'Deposit'
-      },
-      {
-        date: '21.02.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 21140.25,
-        type: 'Withdrawal'
-      },
-      {
-        date: '21.01.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 9919.01,
-        type: 'Deposit'
-      },
-      {
-        date: '21.02.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 140.25,
-        type: 'Withdrawal'
-      },
-      {
-        date: '21.01.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 467.01,
-        type: 'Deposit'
-      },
-      {
-        date: '21.02.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 9812,
-        type: 'Withdrawal'
-      },
-      {
-        date: '21.01.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 999.01,
-        type: 'Deposit'
-      },
-      {
-        date: '21.02.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 21140.25,
-        type: 'Withdrawal'
-      },
-      {
-        date: '21.01.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 999.01,
-        type: 'Deposit'
-      },
-      {
-        date: '21.02.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 21140.25,
-        type: 'Withdrawal'
-      },
-      {
-        date: '21.01.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 999.01,
-        type: 'Deposit'
-      },
-      {
-        date: '21.02.2024',
-        walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
-        ammount: 21140.25,
-        type: 'Withdrawal'
-      }
-    ]
-  }),
-  components: {
-    UserHistoryTx
+defineOptions({
+  name: 'UserTx'
+})
+const columns = ref([
+  {
+    name: 'date',
+    required: true,
+    label: 'Дата',
+    align: 'left',
+    field: 'date',
+    format: val => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'walletFrom',
+    align: 'center',
+    label: 'От кого',
+    field: 'walletFrom',
+    sortable: true
+  },
+  {
+    name: 'walletTo',
+    align: 'center',
+    label: 'Куда',
+    field: 'walletTo',
+    sortable: true
+  },
+  {
+    name: 'amount',
+    label: 'Сумма',
+    field: 'amount',
+    sortable: true
+  },
+  {
+    name: 'type',
+    label: 'Тип',
+    field: 'type',
+    sortable: true
   }
-}
+])
+
+const records = ref([
+  {
+    date: '21.01.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 999.01,
+    type: 'Deposit'
+  },
+  {
+    date: '21.02.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 21140.25,
+    type: 'Withdrawal'
+  },
+  {
+    date: '21.01.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 9919.01,
+    type: 'Deposit'
+  },
+  {
+    date: '21.02.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 140.25,
+    type: 'Withdrawal'
+  },
+  {
+    date: '21.01.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 467.01,
+    type: 'Deposit'
+  },
+  {
+    date: '21.02.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 9812,
+    type: 'Withdrawal'
+  },
+  {
+    date: '21.01.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 999.01,
+    type: 'Deposit'
+  },
+  {
+    date: '21.02.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 21140.25,
+    type: 'Withdrawal'
+  },
+  {
+    date: '21.01.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 999.01,
+    type: 'Deposit'
+  },
+  {
+    date: '21.02.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 21140.25,
+    type: 'Withdrawal'
+  },
+  {
+    date: '21.01.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 999.01,
+    type: 'Deposit'
+  },
+  {
+    date: '21.02.2024',
+    walletFrom: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    walletTo: 'BvF9EUZntQZBsQmKCadDc8XmarSdLGdRS3LGJuUUsSqv',
+    amount: 21140.25,
+    type: 'Withdrawal'
+  }
+])
 </script>
