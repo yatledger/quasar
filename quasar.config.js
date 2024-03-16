57575508aa6b60450c37f55e75c9456f41c4a464
Erop <sha.egor@gmail.com>
2024-03-16 15:36:39 +0300

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
