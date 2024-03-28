784bf6052f2206964bd1c561fe4231b295191cb3
aleksei-shvetz <al.sv.dev@gmail.com>
2024-03-28 15:17:21 +0300

    Create modal for confirm clear device

---
+++ src/layouts/ClearConfirmModal.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  handleConfirm: Function
+})
+</script>
+
+<template>
+  <q-dialog persistent>
+    <q-card>
+      <q-card-section class="row items-center">
+        <q-icon name="error" size="md" color="negative" text-color="white" />
+        <span class="q-ml-sm text-h6">
+          {{ $t('confirmMessages.clearDevice.clearQuestion') }}
+        </span>
+      </q-card-section>
+
+      <q-card-section>
+        {{ $t('confirmMessages.clearDevice.info') }}
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn :label="$t('buttons.cancel')" color="primary" v-close-popup />
+        <q-btn @click="handleConfirm" :label="$t('buttons.confirm')" color="negative" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
