fb4772a12839269d464f5d2a853ee7d75fa044fc
habdevs <habdevs@yandex.ru>
2024-03-28 15:54:54 +0500

    feat: intermediate profile page

--- src/pages/user/UserProfile.vue
+++ src/pages/user/UserProfile.vue
@@ -106,11 +106,3 @@ const send = () => {
   color: $accent;
 }
 </style>
-
-<!-- <template>
-  <q-input label="Имя" placeholder="На балансе должно быть более 100ѣ" variant="underlined"></q-input>
-  <q-input placeholder="Pick a cover" label="Добавьте обложку" hint="На балансе должно быть более 500ѣ"></q-input>
-  <q-input type="textarea" label="Описание" no-resize
-    hint="Для хранения описания держите на балансе более 250ѣ"></q-input>
-  <div class="q-mt-md q-mb-lg"><q-btn color="primary" size="xl" @click="send" label="Отправить" /></div>
-</template> -->
